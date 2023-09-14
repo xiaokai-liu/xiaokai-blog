3c52bc9bcade55e3389cbf3430347da27ea4f3d9
xiaokai-liu <1404197328@qq.com>
2023-09-14 15:53:03 +0800

    feat:配置修改

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,74 +1,29 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
   {
     text: 'css相关',
     icon: "book",
-    prefix: "/css/",
-    children: [
-      {
-        text: "css基础",
-        icon: "pen-to-square",
-        prefix: "base/",
-        children: [
-          { text: "基础", icon: "pen-to-square", link: "1" },
-          { text: "常见需求", icon: "pen-to-square", link: "2" },
-        ],
-      },
-      {
-        text: "css高级",
-        icon: "pen-to-square",
-        prefix: "super/",
-        children: [
-          { text: "css常见布局", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-        ],
-      },
-    ]
-       
+    link: '/css/'
   },
   {
     text: 'js相关',
     icon: "book",
-    prefix: "/js/",
-    children: [
-      {
-        text: "js基础",
-        icon: "pen-to-square",
-        prefix: "初级/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-        ],
-      },
-      {
-        text: "js高级",
-        icon: "pen-to-square",
-        prefix: "高级/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-        ],
-      },
-    ]
+    link: "/js/",
   },
   {
     text: 'vue相关',
     icon: "book",
-    prefix: "/vue/",
-    children: []
+    link: "/vue/",
   },
   {
     text: 'react相关',
     icon: "book",
-    prefix: "/react/",
-    children: []
+    link: "/react/",
   },
   {
     text: '其他',
     icon: "book",
-    prefix: "/other/",
-    children: []
+    link: "/other/",
   },
 ]);
