9f01a0a0ea685032f54ee339a1050c600d369e41
xiaokai-liu <1404197328@qq.com>
2023-10-09 18:07:13 +0800

    feat:js高级相关文档

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -29,12 +29,12 @@ export default sidebar({
         {
           text: '基础',
           prefix: "基础",
-          children: ['1.md','2.md','3.md','4.md','5.md','6.md','7.md','8.md'],
+          children: ['1.md','2.md','3.md','4.md','5.md'],
         },
         {
           text: '高级',
           prefix: "高级",
-          children: ['1.md','2.md','3.md','4.md'],
+          children: ['1.md','2.md','3.md','4.md', '5.md','6.md','7.md','8.md'],
         }
       ],
     }
